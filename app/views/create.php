7f272de577f1c89e760fd27092cbb7c1103bd7e6
Juan <daner_10@live.com.mx>
2023-09-14 08:21:54 -0600

    v5

--- app/views/create.php
+++ app/views/create.php
@@ -24,65 +24,6 @@
         </div>
     </div>
 </div>
-
-<script>
-    $(document).ready(() => {
-        $("#crear").click(() => {
-            let nombre = $("#nombre").val()
-            let telefono = $("#telefono").val()
-            let email = $("#email").val()
-            if (!nombre) {
-                Swal.fire({
-                    icon: "error",
-                    text: "No escribiste un nombre",
-                    timer: 2000
-                })
-                //$("#nombre").focus()
-                return false
-            }
-            if (!telefono) {
-                Swal.fire({
-                    icon: "error",
-                    text: "No escribiste un telefono",
-                    timer: 2000
-                })
-                //$("#nombre").focus()
-                return false
-            }
-            if (!email) {
-                Swal.fire({
-                    icon: "error",
-                    text: "No escribiste un email",
-                    timer: 2000
-                })
-                //$("#nombre").focus()
-                return false
-            }
-            
-            //Validaciones de nombre, email, telefono Sweetalert
-            $.ajax({
-                url: "./app/model/process/create.process.php",
-                data: {
-                    nombre,
-                    telefono,
-                    email,
-                },
-                type: "POST",
-                success: () => { //TAREA
-                    Swal.fire({
-                        title: 'Contacto guardado!',
-                        text: "Contacto guardado con exito!",
-                        icon: 'success',
-                        confirmButtonColor: '#3085d6',
-                        confirmButtonText: 'Ok!'
-                        }).then((result) => {
-                            window.location = "./read"
-                        })  
-            },
-            error:() => {
-                console.log("Error") //Sweetalert Error
-            }
-            })
-        })
-    })
-</script>
+<?php
+    require "./app/controller/create.controller.php"
+?>
--- app/views/update.php
+++ app/views/update.php
@@ -26,38 +26,6 @@
     </div>
 </div>
 
-<script>
-    $(document).ready(() => {
-        $("#update").click(() => {
-            let id = $("#id").val()
-            let nombre = $("#nombre").val()
-            let telefono = $("#telefono").val()
-            let email = $("#email").val()
-            $.ajax({
-                url: "./app/model/process/update.process.php",
-                data: {
-                    id,
-                    nombre,
-                    telefono,
-                    email,
-                },
-                type: "POST",
-                success: (a,b,c) => { //para  checar errores se dan parametros a esta funcion flecha *success: (a,b,c) => {
-                    //console.log(a,b,c)
-                    Swal.fire({
-                        title: 'Contacto actualizado!',
-                        text: "Contacto actualizado con exito!",
-                        icon: 'success',
-                        confirmButtonColor: '#3085d6',
-                        confirmButtonText: 'Ok!'
-                        }).then((result) => {
-                            window.location = "./read"
-                        })  
-            },
-            error:() => {
-                console.log("Error")
-            }
-            })
-        })
-    })
-</script>
+<?php 
+    require "./app/controller/update.controller.php"
+?>
